65c8e3dd775daef598563ac02a3c4fedd4dec782
rafiftrlh <rafifitro89@gmail.com>
2024-08-09 18:53:06 +0700

    fix: contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,92 +1,92 @@
-import { Link } from "react-router-dom";
-import { Footer } from "../sections/Footer";
+import { Link } from 'react-router-dom'
+import { Footer } from '../sections/Footer'
 
 export const ContactPage = () => {
   return (
-    <div className="overflow-hidden relative">
-      <div className="flex overflow-hidden space-x-10 w-screen border-y border-primary opacity-30 absolute top-[40%] rotate-6">
-        <div className="flex whitespace-nowrap py-2 space-x-10 animate-loop-scrol">
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
+    <div className='overflow-hidden relative'>
+      <div className='flex overflow-hidden space-x-10 w-screen border-y border-primary opacity-30 absolute top-[40%] rotate-6'>
+        <div className='flex whitespace-nowrap py-2 space-x-10 animate-loop-scrol'>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
         </div>
         <div
-          aria-hidden="true"
-          className="flex whitespace-nowrap py-2 space-x-10 animate-loop-scrol"
+          aria-hidden='true'
+          className='flex whitespace-nowrap py-2 space-x-10 animate-loop-scrol'
         >
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
-          <span className="text-4xl text-primary font-bold">CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
+          <span className='text-4xl text-primary font-bold'>CONTACT ME</span>
         </div>
       </div>
-      <section>
+      <section className='p-12 md:p-[120px]'>
         <Link
           onClick={() =>
             window.scrollTo({
-              top: 0,
+              top: 0
             })
           }
-          to="/"
-          className="transition-all flex items-center gap-3 text-sm sm:text-base font-semibold hover:text-primary group w-fit"
+          to='/'
+          className='transition-all flex items-center gap-3 text-sm sm:text-base font-semibold hover:text-primary group w-fit'
         >
-          <div className="transition-all border-[1.5px] p-1 rounded-full w-fit h-fit group-hover:border-primary">
+          <div className='transition-all border-[1.5px] p-1 rounded-full w-fit h-fit group-hover:border-primary'>
             <svg
-              className="transition-all stroke-wht group-hover:stroke-primary h-3 w-3 sm:h-4 sm:w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
+              className='transition-all stroke-wht group-hover:stroke-primary h-3 w-3 sm:h-4 sm:w-4'
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
             >
               <path
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                fill='none'
+                strokeLinecap='round'
+                strokeLinejoin='round'
                 strokeWidth={3}
-                d="m14 7l-5 5m0 0l5 5"
+                d='m14 7l-5 5m0 0l5 5'
               ></path>
             </svg>
           </div>
           Home
         </Link>
-        <div className="flex flex-col mt-5 w-full">
-          <h1 className="text-2xl sm:text-3xl font-bold tracking-wide">
+        <div className='flex flex-col mt-5 w-full'>
+          <h1 className='text-2xl sm:text-3xl font-bold tracking-wide'>
             Let&apos;s work together!
           </h1>
-          <p className="text-base sm:text-lg font-medium">
+          <p className='text-base sm:text-lg font-medium'>
             Please fill out this short form and I will contact you.
           </p>
-          <form action="" className="mt-5 flex flex-col gap-5">
-            <input type="text" placeholder="Name" className="input-contact" />
+          <form action='' className='mt-5 flex flex-col gap-5'>
+            <input type='text' placeholder='Name' className='input-contact' />
             <input
-              type="text"
-              placeholder="Email Address"
-              className="input-contact"
+              type='text'
+              placeholder='Email Address'
+              className='input-contact'
             />
             <textarea
-              placeholder="Type Your Message Here"
-              className="textarea-contact"
-              name=""
-              id=""
-              cols="30"
-              rows="10"
+              placeholder='Type Your Message Here'
+              className='textarea-contact'
+              name=''
+              id=''
+              cols='30'
+              rows='10'
             ></textarea>
             <button
-              className="transition-all border-2 border-wht text-wht rounded-full py-4 font-semibold tracking-wider text-base hover:bg-wht hover:text-blk"
-              type="submit"
+              className='transition-all border-2 border-wht text-wht rounded-full py-4 font-semibold tracking-wider text-base hover:bg-wht hover:text-blk'
+              type='submit'
             >
               Submit
             </button>
@@ -95,5 +95,5 @@ export const ContactPage = () => {
       </section>
       <Footer />
     </div>
-  );
-};
+  )
+}
